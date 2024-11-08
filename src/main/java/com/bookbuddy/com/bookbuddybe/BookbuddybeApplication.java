5a055b7d2baf6f1c8f84d294ee0a8b4179cde904
byronholmes2022 <lshopeful2022@gmail.com>
2024-11-08 10:46:59 -0600

    First commit - boilerplate

---
+++ src/main/java/com/bookbuddy/com/bookbuddybe/BookbuddybeApplication.java
@@ -0,0 +1,13 @@
+package com.bookbuddy.com.bookbuddybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbuddybeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookbuddybeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookbuddy/com/bookbuddybe/BookbuddybeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbuddy.com.bookbuddybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbuddybeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
